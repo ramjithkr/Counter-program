f4d42d10662b5284e44756921e237420b92e1ca4
ramjith kr <ramjithkr5441@gmail.com>
2024-03-16 15:22:48 +0530

    finished

---
+++ index.js
@@ -0,0 +1,21 @@
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn    = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLable  = document.getElementById("countLable");
+
+let count = 0;
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLable.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLable.textContent = count;
+}
+
+increaseBtn.onclick = function(){
+    count++;
+    countLable.textContent = count;
+}
